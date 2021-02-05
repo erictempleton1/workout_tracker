85fee702c81e6314417f1d916b1b3ff722a78224
Eric Templeton <erictempleton1@users.noreply.github.com>
2021-02-05 01:33:27 +0000

    init commit

---
+++ workout_project/tracker_app/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView, LogoutView
+
+import tracker_app.views as tracker_views
+
+
+app_name = "tracker_app"
+urlpatterns = [
+  path("", tracker_views.HomeView.as_view(), name="home"),
+  path("signup/", tracker_views.SignupView.as_view(), name="signup"),
+  path("login/", LoginView.as_view(), name="login"),
+  path("logout/", login_required(LogoutView.as_view()), name="logout")
+]
---
+++ workout_project/tracker_app/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
+
+from django.contrib.auth import login
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic.edit import FormView
+
+
+class HomeView(TemplateView):
+  template_name = "tracker_app/home.html"
+
+
+class SignupView(FormView):
+  form_class = UserCreationForm
+  model = User
+  template_name = "registration/signup.html"
+  success_url = reverse_lazy("tracker_app:home")
+
+  def form_valid(self, form):
+    user = form.save()
+    login(self.request, user)
+    return super().form_valid(form)
