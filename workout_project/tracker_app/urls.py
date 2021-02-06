bf2aeeba20b15aebc60bb5d77bd234081ed5783f
Eric Templeton <erictempleton1@users.noreply.github.com>
2021-02-06 11:52:22 +0000

    stubbing things out

--- workout_project/tracker_app/urls.py
+++ workout_project/tracker_app/urls.py
@@ -10,5 +10,9 @@ urlpatterns = [
   path("", tracker_views.HomeView.as_view(), name="home"),
   path("signup/", tracker_views.SignupView.as_view(), name="signup"),
   path("login/", LoginView.as_view(), name="login"),
-  path("logout/", login_required(LogoutView.as_view()), name="logout")
+  path("logout/", login_required(LogoutView.as_view()), name="logout"),
+  path(
+    "<int:pk>/", 
+    tracker_views.ExerciseDetailView.as_view(), 
+    name="exercise_detail")
 ]
--- workout_project/tracker_app/views.py
+++ workout_project/tracker_app/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
-
 from django.contrib.auth import login
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-from django.views.generic.edit import FormView
+from django.views.generic.edit import FormView, CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+
+from tracker_app.models import Exercise
 
 
 class HomeView(TemplateView):
@@ -23,3 +26,19 @@ class SignupView(FormView):
     login(self.request, user)
     return super().form_valid(form)
 
+# TODO - update template with exercise stuff
+class ExerciseDetailView(DetailView):
+  model = Exercise
+  context_object_name = "exercise"
+
+# TODO - add create view, template, and url
+class ExerciseCreateView(CreateView): pass
+
+
+class ExerciseUpdateView(UpdateView): pass
+
+
+class ExerciseDeleteView(DeleteView): pass
+
+
+class ExerciseListView(ListView): pass
