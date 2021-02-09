48521833148c5b6c4e364d36f5620aea2a96e529
Eric Templeton <erictempleton1@users.noreply.github.com>
2021-02-09 01:42:58 +0000

    working on tw stuff

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./workout_project/tracker_app/templates/**/*.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
